425d1f0d773b05c5d2e55d863faee2cd1d3fc4fe
Min-Yang Lee <Min-Yang.Lee@noaa.gov>
2021-03-11 08:52:11 -0500

    add input data to folder creation. remove the "root" part, and just do the file creation relative to the project directory.

--- R_code/project_logistics/setup_new_action.R
+++ R_code/project_logistics/setup_new_action.R
@@ -4,16 +4,20 @@ fmp<-"Herring"
 action<-"Framework7"
 
 # Setup directories for R_code
-dir.create(file.path(root,"R_code",fmp,action), recursive=TRUE)
+dir.create(file.path("R_code",fmp,action), recursive=TRUE)
 
 # Setup directories for images
-dir.create(file.path(root,"images",fmp,action), recursive=TRUE)
+dir.create(file.path("images",fmp,action), recursive=TRUE)
 
 # Setup directories for raw_data
-dir.create(file.path(root,"raw_data",fmp,action),  recursive=TRUE)
+dir.create(file.path("raw_data",fmp,action),  recursive=TRUE)
+
+# Setup directories for raw_data
+dir.create(file.path("input_data",fmp,action),  recursive=TRUE)
+
 
 # Setup directories for stata_code
-dir.create(file.path(root,"stata_code",fmp,action),  recursive=TRUE)
+dir.create(file.path("stata_code",fmp,action),  recursive=TRUE)
 
 # Setup directories for tables
-dir.create(file.path(root,"tables",fmp,action),  recursive=TRUE)
+dir.create(file.path("tables",fmp,action),  recursive=TRUE)
