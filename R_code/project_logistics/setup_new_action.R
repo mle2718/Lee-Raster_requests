d14a6c8d970cf9fc6c8c4551aa68ba4d24d75ff7
mle2718 <Min-Yang.Lee@noaa.gov>
2021-03-15 16:42:35 -0400

    add geotiffs subfolder.

--- R_code/project_logistics/setup_new_action.R
+++ R_code/project_logistics/setup_new_action.R
@@ -1,7 +1,7 @@
 # R script to create folders for a project
 
-fmp<-"RiverHerring"
-action<-"July2021"
+fmp<-"Herring"
+action<-"Framework7"
 
 # Setup directories for R_code
 dir.create(file.path("R_code",fmp,action), recursive=TRUE)
@@ -15,6 +15,8 @@ dir.create(file.path("raw_data",fmp,action),  recursive=TRUE)
 # Setup directories for raw_data
 dir.create(file.path("input_data",fmp,action),  recursive=TRUE)
 
+# Setup directories for geotiffs
+dir.create(file.path("geotiffs",fmp,action),  recursive=TRUE)
 
 # Setup directories for stata_code
 dir.create(file.path("stata_code",fmp,action),  recursive=TRUE)
